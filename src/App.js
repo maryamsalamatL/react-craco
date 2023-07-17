ffcfad0b5bc0275477a8a7051ad3f2e19fa5525d
maryamsalamat <maryamsalamat47@gmail.com>
2023-07-17 11:50:19 +0330

    customize CRA using craco

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Button from "@/component/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
